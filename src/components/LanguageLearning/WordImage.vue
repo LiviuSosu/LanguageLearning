9eda6fe40ab83fe8aa394f167da2ed9418dff895
LiviuSosu <sosuliviu@gmail.com>
2022-05-31 05:51:20 +0200

    a

--- src/components/LanguageLearning/WordImage.vue
+++ src/components/LanguageLearning/WordImage.vue
@@ -1,4 +1,5 @@
 <script>
+import './navigation.css';
 
 export default {
   data() {
@@ -9,5 +10,21 @@ export default {
 </script>
 
 <template>
-<p>aaaa</p>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <a href="#" class="previous">&laquo; Previous</a>
+    </div>
+    <div class="col-sm">
+      <div class="text-center">
+  <img src="https://www.jquery-az.com/html/images/banana.jpg" class="rounded" alt="...">
+</div>
+    </div>
+    <div class="col-sm">
+     <a href="#" class="next">Next &raquo;</a>
+    </div>
+  </div>
+</div>
  </template>
+
+ //https://www.w3schools.com/howto/howto_css_next_prev.asp
