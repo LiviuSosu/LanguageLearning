7e317860cf20257ae7fa0c83ee6a025474e3bf5a
LiviuSosu <sosuliviu@gmail.com>
2022-08-02 20:09:14 +0200

    ?

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -2,14 +2,22 @@
 export default {
   data() {
     return {
-      message: ''
+      model: {
+        word:'',
+        category:''
+      }
     }
-  }
+  },
+   methods: {
+  greet(model) {
+   alert(`Hello `+model.word)
+  }}
 }
 </script>
 
 <template>
   <h3>This is the admin view</h3>
-  <p>Message is: {{ message }}</p>
-	<input v-model="message" placeholder="edit me" />
+  <p>Message is: {{ model.word }}</p>
+	<input v-model="model.word" placeholder="edit me" />
+  <button type="button" class="btn btn-primary" @click="greet(model); value = model" >Primary</button>
  </template>
