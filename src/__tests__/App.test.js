fc5dcff6fd830de85ee815e2b6945f27872f8ed5
LiviuSosu <sosuliviu@gmail.com>
2022-09-20 08:23:29 +0200

    ?

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,16 @@
 //https://www.digitalocean.com/community/tutorials/vuejs-vue-testing
 //https://vitest.dev/guide/
+//https://youtu.be/FJRuG85tXV0?t=744
 
 import { assert, expect, test } from 'vitest'
+import *  as vi from '../__mocks__/axios';
+import {render, screen} from "@testing-library/vue"
+
+//vi.mock('axios')
 
 test('Math.sqrt()', () => {
   expect(Math.sqrt(4)).toBe(2)
   expect(Math.sqrt(144)).toBe(12)
   expect(Math.sqrt(2)).toBe(Math.SQRT2)
 })
+
